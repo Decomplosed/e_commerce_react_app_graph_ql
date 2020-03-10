5e9a3fc9c3350f33a992a6db6f97a680deabd6be
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-10 23:16:34 +0100

    started creating a container for CollectionsOverview component

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+import CollectionsOvierview from './collections-overview.component'
+import Spinner from '../spinner/spinner.component'
