93ea60d9911c15a0a96eae7f3bafc8ba23b120b9
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-10 22:58:47 +0100

    imported ApolloClient

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { ApolloProvider } from 'react-apollo'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-boost'
 
 import { store, persistor } from './redux/store'
 
