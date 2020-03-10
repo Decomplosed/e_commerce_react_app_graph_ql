124cab02ab7b6816e0c30db4774d8062278e081c
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-10 23:17:37 +0100

    created a query for getting collections

--- src/components/collections-overview/collections-overview.container.jsx
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -4,3 +4,18 @@ import { gql } from 'apollo-boost'
 
 import CollectionsOvierview from './collections-overview.component'
 import Spinner from '../spinner/spinner.component'
+
+const GET_COLLECTIONS = gql`
+  {
+    colllections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
