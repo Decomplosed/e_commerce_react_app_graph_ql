7508984fc9e346d75408d0f54163f73feaf53871
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-11 21:19:51 +0100

    created GET_CART_HIDDEN variable

--- src/components/header/header.container.jsx
+++ src/components/header/header.container.jsx
@@ -3,3 +3,11 @@ import { Query } from 'react-apollo'
 import { gql } from 'apollo-boost'
 
 import Header from './header.component'
+
+const GET_CART_HIDDEN = gql`
+  {
+    cartHidden @client
+  }
+`
+
+const HeaderContainer = () => <Query query={GET_CART_HIDDEN}></Query>
