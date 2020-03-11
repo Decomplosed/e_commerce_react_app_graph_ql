1b9c19647b3c478b2908c0e64491836ebfcd896b
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-11 21:58:57 +0100

    cleaned redux inside cart-dropdown component

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -38,7 +38,7 @@ export const resolvers = {
 
       return cartHidden
     },
-    AddItemToCart: (_root, { item }, { cache }) => {
+    addItemToCart: (_root, { item }, { cache }) => {
       const { cartItems } = cache.readQuery({
         query: GET_CART_ITEMS
       })
