0ca2c424c581f5abecc84c89850adfde57350392
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-12 10:14:46 +0100

    created ADD_ITEM_TO_CART mutation

--- src/components/collection-item/collection-item.container.jsx
+++ src/components/collection-item/collection-item.container.jsx
@@ -3,3 +3,9 @@ import { Mutation } from 'react-apollo'
 import { gql } from 'apollo-boost'
 
 import CollectionItem from './collection-item.component'
+
+const ADD_ITEM_TO_CART = gql`
+  mutation AddItemToCart($item: Item!) {
+    addItemToCart(item: $item) @client
+  }
+`
