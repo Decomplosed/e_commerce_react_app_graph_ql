b6e1c49c36058cdfb5e259eb5099a46636cd3413
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-12 12:33:26 +0100

    removed selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import { createStructuredSelector } from 'reselect'
-
-import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
-
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 import './cart-icon.styles.scss'
@@ -13,9 +9,5 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <span className='item-count'>{itemCount}</span>
   </div>
 )
-
-const mapStateToProps = createStructuredSelector({
-  itemCount: selectCartItemsCount
-})
 
 export default CartIcon
