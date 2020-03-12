3b8bd9106441c2be148d1352dabed56ea4efc9d2
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-12 12:30:14 +0100

    imported Query

--- src/components/cart-icon/cart-icon.container.jsx
+++ src/components/cart-icon/cart-icon.container.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Mutation } from 'react-apollo'
+import { Mutation, Query } from 'react-apollo'
 import { gql } from 'apollo-boost'
 
 import CartIcon from './cart-icon.component'
